13520de7a8cafc9bd8311607f9b2f2fe1c0fe893
alincamara9403 <alin.camara@sinoptix.com>
2022-11-01 09:02:45 +0200

    add state to slideshow

--- src/containers/screens/Home/SlideShow/index.tsx
+++ src/containers/screens/Home/SlideShow/index.tsx
@@ -1,5 +1,9 @@
-import React, { ReactElement } from 'react'
-import pie from '../../../../styles/images/photo01.jpg'
+import React, { ReactElement, useState } from 'react'
+import photo1 from '../../../../styles/images/photo01.jpg'
+import photo2 from '../../../../styles/images/photo02.jpg'
+import photo3 from '../../../../styles/images/photo03.jpg'
+import photo4 from '../../../../styles/images/photo04.jpg'
+import photo5 from '../../../../styles/images/photo05.jpg'
 import './index.css'
 
 // interface Props {
@@ -7,25 +11,86 @@ import './index.css'
 // }
 
 export const SlideShow = (): ReactElement => {
+  const [selectedPictureId, setSelectedPictureId] = useState('1')
+
+  const handlePhoto1 = () => {
+    setSelectedPictureId('1')
+  }
+
+  const handlePhoto2 = () => {
+    setSelectedPictureId('2')
+  }
+
+  const handlePhoto3 = () => {
+    setSelectedPictureId('3')
+  }
+
+  const handlePhoto4 = () => {
+    setSelectedPictureId('4')
+  }
+
+  const handlePhoto5 = () => {
+    setSelectedPictureId('5')
+  }
+
   return (
     <div className="slideShow">
       <div className="imageWrapper">
-        <img src={pie} className="pie" alt="pie" />
+        {selectedPictureId === '1' && (
+          <img src={photo1} className="pie" alt="pie" />
+        )}
+        {selectedPictureId === '2' && (
+          <img src={photo2} className="pie" alt="pie" />
+        )}
+        {selectedPictureId === '3' && (
+          <img src={photo3} className="pie" alt="pie" />
+        )}
+        {selectedPictureId === '4' && (
+          <img src={photo4} className="pie" alt="pie" />
+        )}
+        {selectedPictureId === '5' && (
+          <img src={photo5} className="pie" alt="pie" />
+        )}
       </div>
       <div className="slideNav">
-        <div className="circle">
+        <div
+          className="circle"
+          onClick={handlePhoto1}
+          onKeyDown={handlePhoto1}
+          aria-hidden="true"
+        >
           <div className="smallCircle" />
         </div>
-        <div className="circle">
+        <div
+          className="circle"
+          onClick={handlePhoto2}
+          onKeyDown={handlePhoto2}
+          aria-hidden="true"
+        >
           <div className="smallCircle" />
         </div>
-        <div className="circle">
+        <div
+          className="circle"
+          onClick={handlePhoto3}
+          onKeyDown={handlePhoto3}
+          aria-hidden="true"
+        >
           <div className="smallCircle" />
         </div>
-        <div className="circle">
+        <div
+          className="circle"
+          onClick={handlePhoto4}
+          onKeyDown={handlePhoto4}
+          aria-hidden="true"
+        >
           <div className="smallCircle" />
         </div>
-        <div className="circle">
+        <div
+          className="circle"
+          onClick={handlePhoto5}
+          onKeyDown={handlePhoto5}
+          aria-hidden="true"
+        >
           <div className="smallCircle" />
         </div>
       </div>
