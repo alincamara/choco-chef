2587be1df7598ffa050d036ad2c29f4828679e9d
alincamara9403 <alin.camara@sinoptix.com>
2022-11-14 09:56:00 +0200

    add setInterval to slideShow

--- src/containers/screens/Home/SlideShow/index.tsx
+++ src/containers/screens/Home/SlideShow/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import photo1 from '../../../../styles/images/photo01.jpg'
 import photo2 from '../../../../styles/images/photo02.jpg'
 import photo3 from '../../../../styles/images/photo03.jpg'
@@ -12,89 +12,87 @@ import './index.css'
 // }
 
 export const SlideShow = (): ReactElement => {
-  const [selectedPictureId, setSelectedPictureId] = useState('1')
+  const [selectedPictureId, setSelectedPictureId] = useState(1)
+
+  useEffect(() => {
+    let count = 1
+    setInterval(() => {
+      if (count > 5) {
+        setSelectedPictureId(1)
+        count = 1
+      } else {
+        setSelectedPictureId(count)
+        count += 1
+      }
+    }, 2000)
+  }, [])
 
   const handlePhoto1 = () => {
-    setSelectedPictureId('1')
+    setSelectedPictureId(1)
   }
 
   const handlePhoto2 = () => {
-    setSelectedPictureId('2')
+    setSelectedPictureId(2)
   }
 
   const handlePhoto3 = () => {
-    setSelectedPictureId('3')
+    setSelectedPictureId(3)
   }
 
   const handlePhoto4 = () => {
-    setSelectedPictureId('4')
+    setSelectedPictureId(4)
   }
 
   const handlePhoto5 = () => {
-    setSelectedPictureId('5')
+    setSelectedPictureId(5)
   }
 
   return (
     <div className="slideShow">
       <div className="imageWrapper">
-        {selectedPictureId === '1' && (
+        {selectedPictureId === 1 && (
           <img src={photo1} className="pie" alt="pie" />
         )}
-        {selectedPictureId === '2' && (
+        {selectedPictureId === 2 && (
           <img src={photo2} className="pie" alt="pie" />
         )}
-        {selectedPictureId === '3' && (
+        {selectedPictureId === 3 && (
           <img src={photo3} className="pie" alt="pie" />
         )}
-        {selectedPictureId === '4' && (
+        {selectedPictureId === 4 && (
           <img src={photo4} className="pie" alt="pie" />
         )}
-        {selectedPictureId === '5' && (
+        {selectedPictureId === 5 && (
           <img src={photo5} className="pie" alt="pie" />
         )}
       </div>
       <div className="slideNav">
         <div
-          className={join(
-            'circle',
-            selectedPictureId === '1' && 'circleActive'
-          )}
+          className={join('circle', selectedPictureId === 1 && 'circleActive')}
           onClick={handlePhoto1}
         >
           <div className="smallCircle" />
         </div>
         <div
-          className={join(
-            'circle',
-            selectedPictureId === '2' && 'circleActive'
-          )}
+          className={join('circle', selectedPictureId === 2 && 'circleActive')}
           onClick={handlePhoto2}
         >
           <div className="smallCircle" />
         </div>
         <div
-          className={join(
-            'circle',
-            selectedPictureId === '3' && 'circleActive'
-          )}
+          className={join('circle', selectedPictureId === 3 && 'circleActive')}
           onClick={handlePhoto3}
         >
           <div className="smallCircle" />
         </div>
         <div
-          className={join(
-            'circle',
-            selectedPictureId === '4' && 'circleActive'
-          )}
+          className={join('circle', selectedPictureId === 4 && 'circleActive')}
           onClick={handlePhoto4}
         >
           <div className="smallCircle" />
         </div>
         <div
-          className={join(
-            'circle',
-            selectedPictureId === '5' && 'circleActive'
-          )}
+          className={join('circle', selectedPictureId === 5 && 'circleActive')}
           onClick={handlePhoto5}
         >
           <div className="smallCircle" />
