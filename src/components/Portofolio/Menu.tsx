64f9adbbf4d2fe974a4991bdd6fed645f42ed4e1
alincamara9403 <alin.camara@sinoptix.com>
2023-01-21 21:55:46 +0200

    add project alincamara-app

--- src/components/Portofolio/Menu.tsx
+++ src/components/Portofolio/Menu.tsx
@@ -1,33 +1,33 @@
-import Work1 from '../../assets/alegesanatatea.png'
-import Work2 from '../../assets/alin-site.png'
-import Work3 from '../../assets/choco-chef.jpg'
-import Work4 from '../../assets/youtube.png'
+import Product1 from '../../assets/choux-craquelin.jpg'
+import Product2 from '../../assets/alin-site.png'
+import Product3 from '../../assets/alegesanatatea.png'
+import Product4 from '../../assets/youtube.png'
 
 const Menu = [
   {
     id: 1,
-    image: Work3,
+    image: Product1,
     title: 'Started Choco Chef',
     category: 'Cakes',
     link: 'https://choco-chef.alincamara.ro',
   },
   {
     id: 2,
-    image: Work2,
+    image: Product2,
     title: 'My New Site',
     category: 'Choux',
     link: 'https://alincamara.ro',
   },
   {
     id: 3,
-    image: Work1,
+    image: Product3,
     title: 'Alege Sanatatea',
     category: 'Croissants',
     link: 'https://alegesanatatea.ro',
   },
   {
     id: 4,
-    image: Work4,
+    image: Product4,
     title: 'Started new Youtube chanel - Tot Ce Zboara Se Mananca',
     category: 'Gingerbread',
     link: 'https://www.youtube.com/@totcezboarasemananca144',
