66aca8978fe13694de9f8a25bff551d69451bab0
alincamara@yahoo.com <alincamara@yahoo.con>
2023-02-18 10:27:36 +0200

    small fix

--- src/components/Home/ScrollDown.tsx
+++ src/components/Home/ScrollDown.tsx
@@ -5,7 +5,7 @@ import './index.css'
 export const ScrollDown = (): ReactElement => {
   return (
     <div className="scroll__down">
-      <a href="#portofolio" className="mouse__wrapper">
+      <a href="#catalog" className="mouse__wrapper">
         <span className="home__scroll-name">Scroll Down</span>
         <span className="mouse">
           <span className="wheel"></span>
